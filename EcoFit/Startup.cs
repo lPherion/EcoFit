53e007bdd78d49368db08c3e5a50345c85be2742
alons <alons@DESKTOP-HCS5CNU>
2022-11-07 00:46:32 -0300

    Agregar archivos de proyecto.

---
+++ EcoFit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EcoFit.Startup))]
+namespace EcoFit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
